a3f58cc0e51eee8ec731edf9051f133b8dc29b0f
Jake Runzer <jakerunzer@gmail.com>
2021-02-18 15:07:06 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get("/", async (req, res) => {
   console.log(`[${new Date().toISOString()}] Hit!`);
-  res.send("Hello World!");
+  res.send("Hello from my-branch!");
 });
 
 app.listen(port, () => {
